67447f6a8a8a95d532205d66f9c90e96f4b5541f
Isabelle <isagoubinskaya@gmail.com>
2022-11-16 20:11:14 +0100

    secondex

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,18 @@ public class Main {
         if (birthday < 18){
             System.out.println("you have to wait a little");
         }
+
+        byte age = 7;
+        if (age >= 7){
+            System.out.println("go to school");
+        }
+        if (age >=18){
+            System.out.println("go to university");
+        }
+        if (age>=24){
+            System.out.println("time to search your first job");
+        }
+
+
     }
 }
