d8825d620b52e25e4203e0c23de168e88ed8ced3
Isabelle <isagoubinskaya@gmail.com>
2022-11-16 19:55:29 +0100

    firtex

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        byte birthday = 18;
+        if (birthday >= 18){
+            System.out.println("congratulation for your majority");
+        }
+        if (birthday < 18){
+            System.out.println("you have to wait a little");
+        }
+    }
+}
