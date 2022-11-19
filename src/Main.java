2596d70b23d1a9048ce76c3c33828e60f4bae776
Isabelle <isagoubinskaya@gmail.com>
2022-11-19 20:50:08 +0100

    fourex fivex sixex

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        // chap1
+
         byte birthday = 18;
         if (birthday >= 18){
             System.out.println("congratulation for your majority");
@@ -9,13 +11,13 @@ public class Main {
         }
 
         byte age = 7;
-        if (age >= 7){
+        if (age <= 7){
             System.out.println("go to school");
         }
-        if (age >=18){
+        if (age <=18){
             System.out.println("go to university");
         }
-        if (age>=24){
+        if (age<=24){
             System.out.println("time to search your first job");
         }
 
@@ -29,6 +31,37 @@ public class Main {
         if (placeUsed == 102){
             System.out.println("there are no place for you");
         }
+
+        // chap2
+
+        byte birthday1 = 18;
+        if (birthday1 >= 18){
+            System.out.println("congratulation for your majority");
+        } else {
+            System.out.println("you have to wait a little");
+        }
+
+        byte age1 = 25;
+        if (age1 <= 7){
+            System.out.println("go to school");
+        } else if (age1 <=18) {
+            System.out.println("go to university");
+        } else {
+            System.out.println("time to search your first job");
+        }
+
+
+        byte placeUsed1 = 66;
+        if (placeUsed1 <= 60){
+            System.out.println("you can take a seat");
+        } else if (placeUsed1 > 60){
+            System.out.println("you stay up hohoho");
+        } else {
+            System.out.println("there are no place for you");
+        }
+
+
+
 
 
     }
